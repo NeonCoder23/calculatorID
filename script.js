3eb34cdbda7252c142c9e9cdc79b42b9b287ebcc
NeonCoder23 <142743101+NeonCoder23@users.noreply.github.com>
2023-10-13 21:38:36 +0500

    Calculator
    calculator project in html css javascript (part 1)

---
+++ script.js
@@ -0,0 +1,28 @@
+// function theme1() {
+//     let calc = document.querySelector('.calculator');
+//     let inputs = document.querySelectorAll('.calculator input');
+
+//     calc.classList.add('theme_one');
+//     inputs.forEach(e => {
+//         e.classList.add('theme_one')
+//     })
+// }
+// function theme2() {
+//     let calc = document.querySelector('.calculator');
+//     let inputs = document.querySelectorAll('.calculator input');
+
+//     calc.classList.add('theme_two');
+//     inputs.forEach(e => {
+//         e.classList.add('theme_two')
+//     })
+// }
+// function runThemes() {
+//     let radio1 = document.querySelector('#one');
+//     let radio2 = document.querySelector('#two');
+//     let radio3 = document.querySelector('#three');
+
+//     radio1.addEventListener('click', theme1)
+//     radio2.addEventListener('click', theme2)
+//     radio1.ch
+// }
+// runThemes()
