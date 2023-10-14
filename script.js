81c87338f6f1b5fa6ff7578d1e718d7c9717a4a8
NeonCoder23 <142743101+NeonCoder23@users.noreply.github.com>
2023-10-14 22:03:56 +0500

    Theme change

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ function runThemes() {
     let inputs = document.querySelectorAll('input[type="button"]');
     let inputDisplay = document.querySelector('input[type="text"]');
     let a = 8;
-    let bgClrs = ['#800080', '#dc143c', '#008000', '#000080', '#000035','#008080', '#ffff00', '#228b22', '#c0c0c0'];
-    let txtClrs = ['yellow', 'lime', 'chartreuse', 'orange', 'purple', 'azure', 'navy', 'cyan', 'brown']
+    let bgClrs = ['#800080', '#dc143c', '#008000', '#000080', '#000035','#008080', '#ffff00', '#228b22', '#ff0000'];
+    let txtClrs = ['yellow', 'lime', 'chartreuse', 'orange', 'purple', 'azure', 'navy', 'cyan', 'snow']
 
     for (let i = 0; i <= a; i++){
         radios[i].onclick = () => {
